5d0899b75d8477aa0916dedf8e52ece5f785b97c
ReedPosehn <reedpo@hotmail.ca>
2024-02-27 16:19:14 -0600

    Updated and added test cases.

--- test/test_archive.py
+++ test/test_archive.py
@@ -6,9 +6,23 @@ class UnitTests(unittest.TestCase):
 
     def test_convert_to_old_reddit(self):
         self.assertEqual(
-            convertToOldReddit("https://www.reddit.com"),
+            convert_to_old_reddit("https://www.reddit.com"),
             "https://old.reddit.com",
             "Failed to modify the URL properly.",
+        )
+
+    def test_illegal_characters(self):
+        self.assertEqual(
+            illegal_characters("abcdefghjklmnopqrstuvwxyz", 0),
+            "abcdefghjklmnopqrstuvwxyz",
+        )
+        self.assertEqual(
+            illegal_characters("abcdefghjklmnopqrstuvwxy*", 0),
+            "abcdefghjklmnopqrstuvwxy",
+        )
+        self.assertEqual(
+            illegal_characters("abcdefghjklmnopqrstuvwxy?", 1),
+            "abcdefghjklmnopqrstuvwxy-",
         )
 
 
